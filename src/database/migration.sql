9861ec289dcfdd30cac94bc8e7a832e293ac92e1
Sean Cantwell <cantwellsean0127@gmail.com>
2024-01-19 10:00:26 -0600

    Update database ERD and migration file

--- src/database/migration.sql
+++ src/database/migration.sql
@@ -1,23 +1,26 @@
 DROP TABLE IF EXISTS commands;
 DROP TABLE IF EXISTS flashcards;
 DROP TABLE IF EXISTS flashcard_categories;
+DROP TABLE IF EXISTS users_flashcards;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS sessions;
 CREATE TABLE commands (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     command TEXT NOT NULL,
     description TEXT NOT NULL
 );
 CREATE TABLE flashcard_categories (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     category TEXT NOT NULL
 );
 CREATE TABLE flashcards (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     category_id BIGSERIAL REFERENCES flashcard_categories(id) NOT NULL,
     word TEXT NOT NULL,
     definition TEXT NOT NULL
 );
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL
 );
@@ -26,3 +29,9 @@ CREATE TABLE sessions (
     sess JSON NOT NULL,
     expire TIMESTAMP(6) NOT NULL
 ) WITH (OIDS = FALSE);
+CREATE TABLE users_flashcards (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id BIGSERIAL NOT NULL REFERENCES users(id),
+    flashcard_id BIGSERIAL NOT NULL REFERENCES flashcards(id),
+    status TEXT NOT NULL
+);
