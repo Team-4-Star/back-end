a7e160485f90eeadc897b2000d6d4d49251f2105
Sean Cantwell <cantwellsean0127@gmail.com>
2024-01-19 14:35:13 -0600

    Fix error where users cannot access flashcard category routes

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -2,12 +2,14 @@ import addCommandRoutes from "./command-routes.js"
 import addFlashcardRoutes from "./flashcard-routes.js"
 import addAuthenticationRoutes from "./authentication-routes.js"
 import addAuthorizationRoutes from "./authorization-routes.js"
+import addFlashCardCategoryRoutes from "./flashcard-category-routes.js"
 
 // Applies routes to the server instance
 export default (server) => {
 
     addCommandRoutes(server)
     addFlashcardRoutes(server)
+    addFlashCardCategoryRoutes(server)
     addAuthenticationRoutes(server)
     addAuthorizationRoutes(server)
 
