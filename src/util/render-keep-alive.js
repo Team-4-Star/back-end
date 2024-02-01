6c07f123883db6c071d3878a1dcadb931d45baa2
Sean Cantwell <103895364+cantwellsean0127@users.noreply.github.com>
2024-02-01 10:13:04 -0600

    Add front-end to render keep alive function

--- src/util/render-keep-alive.js
+++ src/util/render-keep-alive.js
@@ -1,21 +1,25 @@
 // The process module allows us to interact with the OS process running this application
+import { url } from "inspector"
 import { env } from "process"
 
 // This function sends a request to the web application in order to prevent the site from shutting down due to inactivity
 export const renderKeepAlive = () => {
 
     // Determines the URL to send requests to
-    let url = undefined
+    let urls = []
+    urls.push(env.front_end_origin)
     if (env.environment === "production") {
-        url = "https://blue-ocean-back-end-production.onrender.com/"
+        urls.push("https://blue-ocean-back-end-production.onrender.com/")
     } else {
-        url = "https://blue-ocean-back-end.onrender.com"
+        urls.push("https://blue-ocean-back-end.onrender.com")
     }
 
-    // Sends a request every 10 minutes
-    setInterval(() => {
-        fetch(url)
-    }, 10 * 60 * 1000)
+    // Sends the requests every 10 minutes
+    for (const url of urls) {
+        setInterval(() => {
+            fetch(url)
+        }, 10 * 60 * 1000)
+    }
 
 }
 
